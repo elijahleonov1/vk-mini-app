70b5c8ea4e0d12778531418178a084f74a3f6d31
elijah <ex@gmail.com>
2019-10-19 14:16:18 +0300

    asdasd

---
+++ precache-manifest.db4647d315c0db515914948309d87c59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21270d75851349cf729dafa3e0505f9",
+    "url": "/vk-mini-app/index.html"
+  },
+  {
+    "revision": "ad862db20777e325de3c",
+    "url": "/vk-mini-app/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "9c8f2e22a7bb95ebca1c",
+    "url": "/vk-mini-app/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "ad862db20777e325de3c",
+    "url": "/vk-mini-app/static/js/2.b639cf67.chunk.js"
+  },
+  {
+    "revision": "9c8f2e22a7bb95ebca1c",
+    "url": "/vk-mini-app/static/js/main.9c549c07.chunk.js"
+  },
+  {
+    "revision": "19c2e7d04cb3b682681c",
+    "url": "/vk-mini-app/static/js/runtime-main.3beddd86.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-mini-app/static/media/persik.4e1ec840.png"
+  }
+]);
